824c01bdcf71881ada58a19088557a66086ce6b2
Vedat <vcinar2124@gmail.com>
2023-03-13 21:44:04 +0100

    School Managment System Basics

---
+++ Runner.java
@@ -0,0 +1,10 @@
+package A01_Projects;
+
+
+import static A01_Projects.Islemler.girisPaneli;
+
+public class Runner {
+    public static void main(String[] args) {
+        girisPaneli();//static method import edildi
+    }
+}
